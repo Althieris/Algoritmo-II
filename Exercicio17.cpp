592cac4b53ea2f813eada3869ef4bce0cce9c3a1
Althieris <43865621+Althieris@users.noreply.github.com>
2018-11-25 05:57:20 -0200

    Exercícios_Lista_AlgII
    Ok.

---
+++ Exercicio17.cpp
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+#include"pch.h"
+#include<iostream>
+
+
+int main ()
+{
+	printf("           *\n");
+	printf("           X\n");
+	printf("          XXX\n");
+	printf("         XXOXX\n");
+	printf("        XXXXXXX\n");
+	printf("       XOXXXXXXX\n");
+	printf("      XXXXXXXXXXL\n");
+	printf("     XXXXXXXXXXXLX\n");
+	printf("    XXOXXXXXXXXXXXX\n");
+	printf("          XX\n");
+	printf("          XX\n");
+	printf("         XXXX\n");
+
+	system("pause");
+	return 0;
+
+}
