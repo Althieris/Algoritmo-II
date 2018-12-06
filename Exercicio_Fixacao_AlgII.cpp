786f3b01c6af52198ced3c8bb0d37f76cfa88160
Althieris <43865621+Althieris@users.noreply.github.com>
2018-12-06 15:17:57 -0200

    exercicios

---
+++ Exercicio_Fixacao_AlgII.cpp
@@ -0,0 +1,219 @@
+// Exercicio_Fixacao_AlgII.cpp : Este arquivo contém a função 'main'. A execução do programa começa e termina ali.
+//
+
+#include "pch.h"
+#include <iostream>
+
+1. Construa um algoritmo que, tendo como dados de entrada dois pontos quaisquer no
+plano, P(x1, y1) e P(x2, y2), escreva a distância entre eles.A fórmula que efetua tal
+cálculo é :
+2. Escreva um algoritmo que leia três números inteiros e positivos(A, B, C) e calcule a
+seguinte expressão :
+, onde
+3. Faça um algoritmo que leia a idade de uma pessoa expressa em dias e mostre - a
+expressa em anos, meses e dias.
+4. Faça um algoritmo que leia as 3 notas de um aluno e calcule a média final deste
+aluno.Considerar que a média é ponderada e que o peso das notas é : 2, 3 e 5,
+respectivamente.
+5. O custo ao consumidor de um carro novo é a soma do custo de fábrica com a
+percentagem do distribuidor e dos impostos(aplicados ao custo de fábrica).
+Supondo que a percentagem do distribuidor seja de 28 % e os impostos de 45 % ,
+escrever um algoritmo que leia o custo de fábrica de um carro e escreva o custo ao
+consumidor.
+6. Dado um número inteiro positivo n, calcular a soma dos n primeiros números inteiros
+positivos.
+7. Elaborar um algoritmo que lê 2 valores a e b e os escreve com a mensagem : "São
+múltiplos" ou "Não são múltiplos".
+8. Elabore um algoritmo que dada a idade de um nadador classifica - o em uma das
+seguintes categorias :
+Infantil A 5 - 7 anos
+Infantil B 8 - 10 anos
+Juvenil A 11 - 13 anos
+Juvenil B 14 - 17 anos
+Adulto maiores de 18 anos
+9. Um usuário deseja um algoritmo onde possa escolher que tipo de média deseja
+calcular a partir de 3 notas.Faça um algoritmo que leia as notas, a opção escolhida
+pelo usuário e calcule a média.
+1 - aritmética
+2 - ponderada(3, 3, 4)
+3 - harmônica
+10. Um vendedor necessita de um algoritmo que calcule o preço total devido por um
+cliente.O algoritmo deve receber o código de um produto e a quantidade comprada
+e calcular o preço total, usando a tabela abaixo :
+Código do Produto Preço unitário
+1001 5, 32
+1324 6, 45
+6548 2, 37
+987 5, 32
+7623 6, 45
+11. Faça um algoritmo para calcular a área de uma circunferência, considerando a
+fórmula AREA = π * RAIO
+2
+.Utilize as variáveis AREA e RAIO, a constante π(pi =
+	3.14159) e os operadores aritméticos de multiplicação.
+	12. Faça um algoritmo que calcule a área de um triângulo, considerando a fórmula
+	Area = .Utilize as variáveis AREA, BASE e ALTURA e os operadores 2
+	BASE * ALTURA
+	aritméticos de multiplicação e divisão.
+	13. Dado um número inteiro positivo n, imprimir os n primeiros naturais ímpares.
+	Exemplo: Para n = 4 a saída deverá ser 1, 3, 5, 7.
+	14. Escrever um programa que mostre a seguinte figura no alto da tela :
+XXXXX
+X X
+X X
+X X
+XXXXX
+15. Dados um inteiro x e um inteiro não - negativo n, calcular x
+n
+.
+16. Faça uma função que leia uma string(vetor char) e retorne a posição(índice) de um
+caracter dentro dessa string.
+17. Implemente um programa que desenhe um "pinheiro" na tela, similar ao
+abaixo.(Enriqueça o desenho com outros caracteres, simulando enfeites)
+X
+XXX
+XXXXX
+XXXXXXX
+XXXXXXXXX
+XXXXXXXXXXX
+XXXXXXXXXXXXX
+XXXXXXXXXXXXXXX
+XX
+XX
+XXXX
+18. Crie um programa que leia um número entre 2 e 20 e gere uma tela com a seguinte
+configuração :
+Digite um número :
+4
+Saida do programa :
+	1
+	12
+	123
+	1234
+	123
+	12
+	1
+	19. Crie uma função capaz de multiplicar uma linha de uma matriz[3][3] por um número
+	informado pelo usuário.Faça o mesmo para uma coluna da mesma matriz.
+	20. Crie uma função capaz de substituir todos os números negativos de uma matriz por
+	seu módulo.
+	21. Dados n(quantidade de números que serão digitados) e depois a sequência de n
+	números inteiros, determinar a soma dos números que são pares.
+	22. Escreva um programa que lê uma matriz quadrada 3x3 e apresenta na tela a sua
+	matriz transposta.
+	23. Faça um algoritmo que calcule a quantidade de litros de combustível gasta em uma
+	viagem, utilizando um automóvel que faz 12Km por litro.Para obter o cálculo, o
+	usuário deve fornecer o tempo gasto na viagem e a velocidade média durante ela.
+	Desta forma, será possível obter a distância percorrida com a fórmula DISTANCIA =
+	TEMPO * VELOCIDADE.Tendo o valor da distância, basta calcular a quantidade de
+	litros de combustível utilizada na viagem com a fórmula : LITROS_USADOS =
+	DISTANCIA / 12. O programa deve apresentar os valores da velocidade média,
+	tempo gasto na viagem, a distância percorrida e a quantidade de litros utilizada na
+	viagem.
+	24. Faça um algoritmo que leia dois valores para as variáveis A e B e efetue a troca dos
+	valores de forma que a variável A passe a possuir o valor da variável B e a variável
+	B passe a possuir o valor da variável A.Apresente os valores trocados.
+	25. Considere a seguinte situação : descontam - se inicialmente 10 % do salário bruto do
+	trabalhador como contribuição à previdência social.Após esse desconto, há um
+	outro desconto de 5 % sobre o valor restante do salário bruto, a título de um
+	determinado imposto.Faça um algoritmo que leia o salário bruto de um cidadão e
+	imprima o seu salário líquido.
+	26. Escreva um algoritmo que determine o número de dias que uma pessoa já viveu.
+	Considere que um mês tenha 30 dias.
+	27. Suponha que um caixa disponha apenas de notas de 1, 10 e 100 reais.
+	Considerando que alguém está pagando uma compra, escreva um algoritmo que
+	mostre o número mínimo de notas que o caixa deve fornecer como troco.Mostre
+	também : o valor da compra, o valor do troco e a quantidade de cada tipo de nota do
+	troco.Suponha que o sistema monetário não utilize moedas.
+	28. A revendedora de carros Pica - Pau Ltda.paga aos seus funcionários vendedores
+	dois salários mínimos fixos, mais uma comissão fixa de R$ 50, 00 por carro vendido e
+	mais 5 % do valor das vendas.Faça um algoritmo que determine o salário total de
+	um vendedor.
+	29. Dados três valores X, Y e Z, verificar se eles podem ser os comprimentos dos lados
+	de um triângulo, e se forem, verificar se é um triângulo equilátero, isóscele ou
+	escaleno.Se eles não formarem um triângulo, escrever uma mensagem.
+	Antes da elaboração do algoritmo, torna - se necessário a revisão de algumas
+	propriedades e definições.
+	Propriedade – o comprimento de cada lado de um triângulo é menor do que a soma
+	dos comprimentos dos outros dois lados.
+	Definição 1 - chama - se de triângulo equilátero o que tem os comprimentos dos três
+	lados iguais;
+Definição 2 - chama - se de triângulo isóscele o triângulo que tem os comprimentos
+de dois lados iguais;
+Definição 3 - chama - se triângulo escaleno o triângulo que tem os comprimentos dos
+três lados diferentes.
+30. Crie um programa que leia um número entre 2 e 20 e gere uma tela com a seguinte
+configuração :
+Digite um número :
+7
+Saida do programa :
+	1234567
+	x123456
+	xx12345
+	xxx1235
+	xxxx123
+	xxxxx12
+	xxxxxx1
+	31. Escrever um algoritmo que lê 5 valores para a, um de cada vez, e conta quantos
+	destes valores são negativos, escrevendo esta informação.
+	32. A prefeitura de uma cidade fez uma pesquisa entre seus habitantes, coletando
+	dados sobre o salário e número de filhos.A prefeitura deseja saber :
+● a) média do salário da população;
+● b) média do número de filhos;
+● c) maior salário;
+● d) percentual de pessoas com salário até R$100, 00.
+O final da leitura de dados se dará com a entrada de um salário negativo. (Use o
+	comando WHILE)
+	33. Chico tem 1, 50 metro e cresce 2 centímetros por ano, enquanto Zé tem 1, 10 metro e
+	cresce 3 centímetros por ano.Construa um algoritmo que calcule e imprima quantos
+	anos serão necessários para que Zé seja maior que Chico.
+	34. Escreva um algoritmo que leia 50 valores e encontre o maior e o menor deles.
+	Mostre o resultado.
+	35. Foi realizada uma pesquisa de algumas características físicas da população de uma
+	certa região, a qual coletou os seguintes dados referentes a cada habitante para
+	serem analisados :
+-idade
+- sexo(masculino e feminino)
+- cor dos olhos(1 - azuis, 2 - verdes ou 3 - castanhos)
+- cor dos cabelos(1 - louros, 2 - castanhos, 3 - pretos)
+Faça um algoritmo que determine e escreva :
+-a maior idade dos habitantes
+- a quantidade de indivíduos do sexo feminino cuja idade está entre 18 e 35 anos
+inclusive e que tenham olhos verdes e cabelos louros.
+O final do conjunto de habitantes é reconhecido pelo valor - 1 entrada como idade.
+36. Escrever um algoritmo que leia 20 valores para uma variável n e, para cada um
+deles, calcule a tabuada de 1 até n.Mostre a tabuada na forma :
+1 x n = n
+2 x n = 2n
+3 x n = 3n
+.......
+n x n = n
+2
+37. Escreva uma função que receba dois structs do tipo dma, cada um representando
+uma data válida, e devolva o número de dias que decorreram entre as duas datas.
+struct dma {
+	int dia;
+	int mes;
+	int ano;
+};
+38. Escreva uma função que receba um número inteiro que representa um intervalo de
+tempo medido em minutos e devolva o correspondente número de horas e minutos
+(por exemplo, converte 131 minutos em 2 horas e 11 minutos).Use uma struct como
+a seguinte :
+struct hm {
+	int horas;
+	int minutos;
+};
+39. Escrever um programa que cadastre o nome, a altura, o peso, o cpf e sexo de
+algumas pessoas.Com os dados cadastrados, em seguida localizar uma pessoas
+através do seu CPF e imprimir o seu IMC.
+40. Um vendedor necessita de um algoritmo que calcule o preço total devido por um
+cliente.O algoritmo deve receber o código de um produto e a quantidade comprada
+e calcular o preço total(deve receber estas informações até o usuário digitar - 1 para
+	o código) e apresentar o total da compra, usando a tabela abaixo :
+Código do produto Preço unitário
+1001 5, 32
+1324 6, 45
+6548 2, 37
+0987 5, 32
+7623 6, 45
